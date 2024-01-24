9899eb62c3aed88513602af1545dbcbc7b844c23
Franco Aquistapace <francoaquistapace@192.168.1.50>
2024-01-24 22:35:59 +0100

    Initialized layout script with Light class, which is used to connect the emulated components with the GUI. Also added clock light.

---
+++ layout.py
@@ -0,0 +1,46 @@
+# In this file we specify some layout elements 
+# for the main emulator window
+
+# Import modules
+import tkinter as tk
+
+# Global params
+LIGHT_R = 10
+
+# Define light class
+class Light(object):
+    def __init__(self, x, y, r, on_c, off_c):
+        self.x = x
+        self.y = y
+        self.r = r
+        self.state = 0
+        self.off_c = off_c
+        self.on_c = on_c
+
+    def build(self, canvas):
+        if self.state == 0:
+            color = self.off_c
+        else:
+            color = self.on_c
+        self.obj = canvas.create_oval(
+                                self.x - self.r, 
+                                self.y - self.r, 
+                                self.x + self.r, 
+                                self.y + self.r, 
+                                fill=color)
+
+    def toggle(self, canvas):
+        if self.state == 0:
+            self.state += 1
+            color = self.on_c
+        else:
+            self.state -= 1
+            color = self.off_c
+        # Update canvas object
+        canvas.itemconfig(self.obj, fill=color)
+
+# Define clock light
+clock_on_c = '#eff5f9'
+clock_off_c = '#b0cfe4'
+clock_light_meta = Light(x=355, y=70, r=LIGHT_R, 
+                        off_c=clock_off_c, on_c=clock_on_c)
--- main.py
+++ main.py
@@ -1,27 +1,38 @@
+# In this file we specify the main script for
+# running the application
+
+# Import modules
 import tkinter as tk
 from tkinter import PhotoImage
 from PIL import Image, ImageTk
 
-def main():
-    root = tk.Tk()
-    root.title("Tkinter Background Image Example")
+# Import layout
+from layout import *
 
+
+def main():
+    # Init window
+    root = tk.Tk()
+    root.title("Ben Eater's 8-bit CPU")
     root.geometry("1244x700")
 
     # Load the image file using Pillow
     image_pil = Image.open("background_img.png")
     image = ImageTk.PhotoImage(image_pil)
 
-    # Create a label with the image as the background
-    background_label = tk.Label(root, image=image)
-    background_label.place(relwidth=1, relheight=1)
+    # Create a canvas
+    canvas_bg = tk.Canvas(root, width=1244, height=700)
+    canvas_bg.pack()
+    # Place the background image on the canvas
+    canvas_bg.create_image(0, 0, anchor=tk.NW, image=image)
 
-    # Add other widgets on top of the background
-    label = tk.Label(root, text="Hello, Tkinter!", font=("Helvetica", 16))
-    label.pack(pady=20)
+    # Draw clock light
+    clock_light_meta.build(canvas_bg)
 
-    button = tk.Button(root, text="Click Me", command=lambda: print("Button Clicked"))
-    button.pack()
+    # Create a button and add it on top of the background image
+    button = tk.Button(root, text="Click Me", 
+                       command=lambda: clock_light_meta.toggle(canvas_bg))
+    button_window = canvas_bg.create_window(400, 300, anchor="center", window=button)
 
     root.mainloop()
 
